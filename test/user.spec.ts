00a7fb22168817aa890d437a8f7eb9e940f2b3ab
Afrizal Sebastian <sebastiangurning@gmail.com>
2024-09-03 20:40:35 +0700

    test: user post register

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -4,23 +4,35 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { Logger } from 'winston';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { TestService } from './test.service';
+import { TestModule } from './test.module';
 
 describe('UserController', () => {
   let app: INestApplication;
   let logger: Logger;
+  let testService: TestService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, TestModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
 
     logger = app.get(WINSTON_MODULE_PROVIDER);
+    testService = app.get(TestService);
   });
 
   describe('POST /api/users', () => {
+    beforeEach(async () => {
+      await testService.deleteUser();
+    });
+
+    afterEach(async () => {
+      await testService.deleteUser();
+    });
+
     it('should be rejected if request invalid', async () => {
       const response = await request(app.getHttpServer())
         .post('/api/users')
@@ -28,6 +40,37 @@ describe('UserController', () => {
           username: '',
           password: '',
           name: '',
+        });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it('should be able to register', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'test',
+          password: 'test',
+          name: 'test',
+        });
+
+      logger.info(response.body);
+
+      expect(response.status).toBe(201);
+      expect(response.body.data.username).toBe('test');
+      expect(response.body.data.name).toBe('test');
+    });
+
+    it('should be rejected when username exists', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'test-exist',
+          password: 'test-exist',
+          name: 'test-exist',
         });
 
       logger.info(response.body);
